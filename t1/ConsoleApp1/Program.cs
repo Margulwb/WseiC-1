2a5fe8ce623efbfcba1674fa3421b891fb8cffa7
Margul <50051075+Margulwb@users.noreply.github.com>
2022-03-05 16:08:50 +0100

    First Commit

---
+++ t1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ulamek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek a = new Ulamek(2, 3);
+            Ulamek b = new Ulamek(1, 2);
+
+            Console.WriteLine(a + b);
+            Console.WriteLine();
+
+            Console.WriteLine(a - b);
+            Console.WriteLine();
+
+            Console.WriteLine(a * b);
+            Console.WriteLine();
+
+            Console.WriteLine(a / b);
+            Console.WriteLine();
+        }
+    }
+}
+/*https://dirask.com/posts/WSEI-2021-2022-lato-labN-1-ISN-Programowanie-obiektowe-lab-1-pa3ek1*/
---
+++ t1/ConsoleApp1/Ulamek.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ulamek
+{
+    class Ulamek
+    {
+        private int licznik;
+
+        private int mianownik;
+
+        Ulamek()
+        {
+            licznik = 1;
+            mianownik = 2;
+        }
+
+        public Ulamek(int l, int m)
+        {
+            this.licznik = l;
+            this.mianownik = m;
+        }
+
+        public Ulamek(Ulamek u)
+        {
+            licznik = u.licznik;
+            mianownik = u.mianownik;
+        }
+
+        public override string ToString() => $"{licznik}\n-\n{mianownik}";
+        public static Ulamek operator +(Ulamek a, Ulamek b) 
+             =>new (a.licznik * b.mianownik + b.licznik * a.mianownik, a.mianownik * b.mianownik);
+
+        public static Ulamek operator -(Ulamek a, Ulamek b)
+            => new Ulamek(a.licznik * b.mianownik - b.licznik * a.mianownik, a.mianownik * b.mianownik);
+        public static Ulamek operator *(Ulamek a, Ulamek b)
+            => new Ulamek(a.licznik * b.licznik, a.mianownik * b.mianownik);
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+            => new Ulamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
+
+    }
+}
